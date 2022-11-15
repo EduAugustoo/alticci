79e1cd817d51ad2764dc02a7eca6a0177692ea76
eduardo <edspinheiro@gmail.com>
2022-11-15 00:26:20 -0300

    feat/Adding redis configuration

--- src/main/java/com/eduardo/alticci/services/impl/AlticciServiceImpl.java
+++ src/main/java/com/eduardo/alticci/services/impl/AlticciServiceImpl.java
@@ -1,8 +1,11 @@
 package com.eduardo.alticci.services.impl;
 
 import com.eduardo.alticci.services.AlticciService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class AlticciServiceImpl implements AlticciService {
 
@@ -10,8 +13,10 @@ public class AlticciServiceImpl implements AlticciService {
     private static final Integer ALTICCI_ONE = 1;
     private static final Integer ALTICCI_TWO = 1;
 
+    @Cacheable("valuesCache")
     @Override
     public Integer calculateAlticciValue(Integer num) {
+        log.info("Sem cache!!");
         switch (num) {
             case 0:
                 return ALTICCI_ZERO;
